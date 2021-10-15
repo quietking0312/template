515156860514a0d0c48e887d14f707cd3420118e
quiet <quiet_king0312@163.com>
2021-10-15 19:34:08 +0800

    script setup

--- web/src/views/dashboard/index.vue
+++ web/src/views/dashboard/index.vue
@@ -1,27 +1,30 @@
 <template>
   <div>
     <div>hello world</div>
-    <svg-icon iconClass="dashboard" />
+    <span>{{ count }}</span>
+    <el-button @click="onClickBtn">点击</el-button>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { ref } from "vue";
 import { PROTO_MESSAGE } from "@/proto/message"
 import { fetch } from "@/request/axios";
 
-export default defineComponent({
-  name: "Dashboard",
-  setup() {
-    const  a = PROTO_MESSAGE.Test.create()
-    a.data = [{time: 1}]
-    console.log(a.toJSON())
-    fetch({url: "v1/helloworld", method: "get"} ).then(res => {
-      console.log(res)
-    })
-    return {}
+
+
+  const  a = PROTO_MESSAGE.Test.create()
+  a.data = [{time: 1}]
+  console.log(a.toJSON())
+  fetch({url: "v1/helloworld", method: "get"} ).then(res => {
+    console.log(res)
+  })
+  let count = ref(0)
+  function onClickBtn() {
+    count.value++
   }
-})
+
+
 </script>
 
 <style scoped>
